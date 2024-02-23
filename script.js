652165c7933b3e7d8c8aac52f4a7a407b7243715
Swaraj Anand <swrjnnd@gmail.com>
2024-02-23 22:29:24 +0530

    Basic dom implimentation

---
+++ script.js
@@ -0,0 +1,12 @@
+let container = document.getElementById("container");
+console.log(container);
+
+container.innerHTML = `Hello, I'm a div!`;
+container.style.backgroundColor = "yellow";
+container.style.margin = "20px";
+container.style.color = "blue";
+container.style.padding = "10px";
+container.style.fontSize = "18px";
+container.style.fontWeight = "bold";
+container.style.height = "200px";
+container.style.width = "300px";
